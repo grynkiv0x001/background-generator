ac3eb3c2ea1da1d7d6d3769188dd4800896782d9
grynkiv0x001 <grynkiv0x001@gmail.com>
2021-06-08 23:36:00 +0300

    Smooth parallax added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const blocksNumber = 140;
+const blocksNumber = 200;
 const container = document.querySelector('.container');
 
 for (let i = 1; i < blocksNumber; i++) {
@@ -9,16 +9,29 @@ for (let i = 1; i < blocksNumber; i++) {
 }
 
 function generate() {
+  document.addEventListener('mousemove', parallax);
+
   anime({
     targets: '.block',
     translateX: function() {
-      return anime.random(-800, 800);
+      return anime.random(-1000, 1000);
     },
     translateY: function() {
-      return anime.random(-600, 600);
+      return anime.random(-800, 800);
     },
     scale: function() {
       return anime.random(1, 5);
     }
   })
+}
+
+function parallax(e) {
+  this.querySelectorAll('.container').forEach(element => {
+    const speed = 2;
+
+    const x = (window.innerWidth - e.pageX * speed) / 100;
+    const y = (window.innerHeight - e.pageY * speed) / 100;
+
+    element.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
 }
